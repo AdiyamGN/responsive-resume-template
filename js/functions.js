954c2781b7c0ecdf3eaf0d0d1490a75ed9c9d73a
Adiyamseged <adiyamgn@outlook.com>
2016-12-01 07:21:03 -0500

    markup and style for youtube section done. added a scroll effect with jquery. made some over all grid changes/restructure the layout.

---
+++ js/functions.js
@@ -0,0 +1,13 @@
+$(window).scroll(function(){
+	youtubeVidScroll();
+});
+
+
+
+
+
+function youtubeVidScroll() {
+	var wScroll = $(window).scrollTop();
+
+	$('.video-strip').css('background-position', 'center -'+ wScroll +'px');
+}
